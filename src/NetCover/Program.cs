2ab4b780c68babaf8d868d03ed570e7ea432fe26
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-04-06 20:45:20 +0100

    Adding NetCover projects.

---
+++ src/NetCover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

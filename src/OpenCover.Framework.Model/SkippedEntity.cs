e40a6c1821d5034302c8d48d30b00139d3198a64
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-04-23 09:10:30 +0100

    Adding license info for OpenCover files that did not have them originally.

--- src/OpenCover.Framework.Model/SkippedEntity.cs
+++ src/OpenCover.Framework.Model/SkippedEntity.cs
@@ -1,3 +1,8 @@
+//
+// OpenCover - S Wilde
+//
+// This source code is released under the MIT License; see the accompanying license file.
+//
 using System.Xml.Serialization;
 
 namespace OpenCover.Framework.Model

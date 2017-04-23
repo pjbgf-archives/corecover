e8933e429dbc8103b04208d7fd5153220b8b2ebf
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-04-23 06:57:37 +0100

    Reading logging settings from configuration file.

--- src/CoreCover.Framework/ExecutionTrackerServer.cs
+++ src/CoreCover.Framework/ExecutionTrackerServer.cs
@@ -6,21 +6,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using CoreCover.Instrumentation;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
 using OpenCover.Framework.Model;
 
 namespace CoreCover.Framework
 {
     public class ExecutionTrackerServer : ExecutionTracker.ExecutionTrackerBase
     {
+        private readonly ILogger _logger;
         private readonly CoverageSession _coverageSession;
 
-        public ExecutionTrackerServer(CoverageSession coverageSession)
+        public ExecutionTrackerServer(CoverageSession coverageSession, ILogger logger = null)
         {
+            _logger = logger;
             _coverageSession = coverageSession;
         }
 
         public override Task<ExecutedLineReply> Track(ExecutedLine request, ServerCallContext context)
         {
+            //TODO: Requires refactoring
             var method = _coverageSession.Modules.First(x => x.ModuleHash == request.ModuleHash)
                 .Classes.First(x => x.Methods.Any(y => y.MetadataToken == request.MetadataToken))
                 .Methods.First(y => y.MetadataToken == request.MetadataToken);
@@ -31,7 +35,7 @@ namespace CoreCover.Framework
             foreach (var sequencePoint in sequencePoints)
                 sequencePoint.VisitCount++;
             
-            Console.WriteLine($"Server: {request.MetadataToken}:{request.StartLineNumber}:{request.EndLineNumber}");
+            _logger?.LogInformation($"gRPC Server Received: {request.MetadataToken}:{request.StartLineNumber}:{request.EndLineNumber}");
             return Task.FromResult(new ExecutedLineReply());
         }
     }
--- src/CoreCover/Program.cs
+++ src/CoreCover/Program.cs
@@ -2,8 +2,11 @@
 // Copyright (c) 2017 Paulo Gomes (https://pjbgf.mit-license.org/)
 
 using System;
+using System.IO;
+using System.Reflection;
 using CoreCover.Framework;
 using CoreCover.Framework.Adapters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace CoreCover
@@ -12,14 +15,27 @@ namespace CoreCover
     {
         public static void Main(string[] args)
         {
-            var logger = new LoggerFactory().AddConsole(LogLevel.Trace).CreateLogger("CoreCover");
-            
+            var logger = GetLogger();
             new ConsoleRunner(
                     logger, new CoverageRunner(
                     new Instrumentator(logger, new CodeCoverageHandler(new CodeInstrumentationHandler(logger))),
                     new DotNetTestRunner(new RpcServer(logger), new Process()), 
                     new OpenCoverReportAdapter()))
                 .ProcessCommand(args);
+        }
+
+        private static ILogger<Program> GetLogger()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
+                .AddJsonFile("appSettings.json", false);
+
+            var config = builder.Build();
+            var loggerFactory = new LoggerFactory()
+                .AddConsole(config.GetSection("Logging"));
+
+            var logger = loggerFactory.CreateLogger<Program>();
+            return logger;
         }
     }
 }

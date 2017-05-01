3b67a904a63f87da098c0eb7b3ce298d75d00ab1
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-01 23:19:38 +0100

    Adding license headers.

--- src/CoreCover.Framework/Abstractions/IRpcServer.cs
+++ src/CoreCover.Framework/Abstractions/IRpcServer.cs
@@ -1,4 +1,7 @@
-﻿using CoreCover.Framework.Model;
+﻿// MIT License
+// Copyright (c) 2017 Paulo Gomes (https://pjbgf.mit-license.org/)
+
+using CoreCover.Framework.Model;
 
 namespace CoreCover.Framework.Abstractions
 {

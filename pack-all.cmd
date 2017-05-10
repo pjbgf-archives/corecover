762f0c847d93e84ba64c053b9f179c8750fcd716
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-10 21:12:20 +0100

    Forcing versions to come out of GitVersion.

--- pack-all.cmd
+++
@@ -1,4 +0,0 @@
-dotnet pack src\CoreCover\CoreCover.csproj %*
-dotnet pack src\CoreCover.Framework\CoreCover.Framework.csproj %*
-dotnet pack src\CoreCover.Instrumentation\CoreCover.Instrumentation.csproj %*
-dotnet pack src\CoreCover.Extensions.OpenCoverReport\CoreCover.Extensions.OpenCoverReport.csproj %*

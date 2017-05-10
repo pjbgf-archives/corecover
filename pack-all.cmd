e1bb630ca153de4a53c7cb706cb476400de05dd4
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-10 21:16:03 +0100

    Fixing command for packing projects.

---
+++ pack-all.cmd
@@ -0,0 +1,4 @@
+dotnet pack src\CoreCover\CoreCover.csproj %*
+dotnet pack src\CoreCover.Framework\CoreCover.Framework.csproj %*
+dotnet pack src\CoreCover.Instrumentation\CoreCover.Instrumentation.csproj %*
+dotnet pack src\CoreCover.Extensions.OpenCoverReport\CoreCover.Extensions.OpenCoverReport.csproj %*
